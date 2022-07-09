4de9268d9fdac5e7dd782ac287e5bfccc11a40cf
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-09 23:46:37 +0530

    [Sharmila] refcator1-generictype

--- Core_Concept_Generics/PrintArray.cs
+++ Core_Concept_Generics/PrintArray.cs
@@ -7,51 +7,10 @@ using System.Threading.Tasks;
 namespace Core_Concept_Generics
 {
     public class PrintArray
-    {
+    { 
 
         //uc1 find maximum of three integers
         public T toPrintMaximum<T>(T value1, T value2, T value3) where T : IComparable
-        {
-            if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0 ||
-                value1.CompareTo(value2) >= 0 && value1.CompareTo(value3) > 0 ||
-                value1.CompareTo(value2) > 0 && value1.CompareTo(value3) >= 0)
-            {
-                return value1;
-            }
-            else if (value2.CompareTo(value1) > 0 && value2.CompareTo(value3) > 0 ||
-               value2.CompareTo(value1) >= 0 && value2.CompareTo(value3) > 0 ||
-               value2.CompareTo(value1) > 0 && value2.CompareTo(value3) >= 0)
-            {
-                return value2;
-            }
-            else
-            {
-                return value3;
-            }
-        }
-        //uc2 find maximum of three float values
-        public double toPrintMaximum_double(double value1, double value2, double value3)
-        {
-            if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0 ||
-                value1.CompareTo(value2) >= 0 && value1.CompareTo(value3) > 0 ||
-                value1.CompareTo(value2) > 0 && value1.CompareTo(value3) >= 0)
-            {
-                return value1;
-            }
-            else if (value2.CompareTo(value1) > 0 && value2.CompareTo(value3) > 0 ||
-               value2.CompareTo(value1) >= 0 && value2.CompareTo(value3) > 0 ||
-               value2.CompareTo(value1) > 0 && value2.CompareTo(value3) >= 0)
-            {
-                return value2;
-            }
-            else
-            {
-                return value3;
-            }
-        }
-
-        //uc3 find maximum of three string values
-        public string toPrintMaximum_String(string value1, string value2, string value3)
         {
             if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0 ||
                 value1.CompareTo(value2) >= 0 && value1.CompareTo(value3) > 0 ||
